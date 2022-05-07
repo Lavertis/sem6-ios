33fe9108237aa5ebcc0c0e946e2265c38b1cbcdb
Lavertis <29688908+Lavertis@users.noreply.github.com>
2022-05-07 20:40:24 +0200

    feat(lab_9): task 1

---
+++ lab_9/zad_9_1/zad_9_1/ContentView.swift
@@ -0,0 +1,68 @@
+//
+//  ContentView.swift
+//  zad_9_1
+//
+//  Created by Rafał Kuźmiczuk on 07/05/2022.
+//  Copyright © 2022 Rafał Kuźmiczuk. All rights reserved.
+//
+
+import SwiftUI
+
+class MyImage {
+    var title: String
+    var path: String
+    
+    init(title: String, path: String) {
+        self.title = title
+        self.path = path
+    }
+}
+
+var images: [MyImage] = [
+    MyImage(title: "Autumn forest", path: "autumn_forest"),
+    MyImage(title: "Riverbed", path: "river"),
+    MyImage(title: "Plane hidden in forest", path: "plane_in_forest"),
+    MyImage(title: "Autumn leaf", path: "autumn_leaf")
+]
+
+struct ContentView: View {
+    @State private var index: Int = 0
+    @State private var showTitle: Bool = false
+    
+    var body: some View {
+        let longPress = LongPressGesture()
+            .onEnded { finished in
+                self.showTitle = !self.showTitle
+            }
+        
+        return VStack {
+            Image(images[index].path)
+                .resizable()
+                .scaledToFit()
+                .gesture(
+                    DragGesture()
+                        .onEnded({ value in
+                            if value.translation.width < 0 && self.index < images.count - 1 {
+                                // left
+                                self.index = self.index + 1
+                            }
+                            if value.translation.width > 0 && self.index > 0 {
+                                // right
+                                self.index = self.index - 1
+                            }
+                        })
+                )
+                .gesture(longPress)
+            
+
+            Text(showTitle ? images[index].title : " ")
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
